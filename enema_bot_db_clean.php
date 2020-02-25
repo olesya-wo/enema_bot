a3d183f542dc0240d2ce18aa6b823d254907a0b4
olesya <olesya.wo@gmail.com>
2020-02-26 00:30:51 +0300

    db_cleaner fix

--- enema_bot_db_clean.php
+++ enema_bot_db_clean.php
@@ -2,6 +2,7 @@
 chdir(__DIR__);
 require_once( 'settings.php' );
 require_once( 'tg_bot_api.php' );
+require_once( 'db_' . $db_module . '.php' );
 
 function send_to_admin( $text ) {
     global $admin_id;
@@ -9,45 +10,49 @@ function send_to_admin( $text ) {
 }
 
 // Подключиться к БД
-$db = new SQLite3( $bot_name . '_db.sqlite3' );
+$db = db_init();
+if ( !$db ) {
+    send_to_admin( 'Error: DB_INIT_FAIL' );
+    exit();
+}
 
 // Сколько всего опросов на момент начала работы клинера
 $sql = "SELECT COUNT(poll_id) FROM polls";
 $res = $db->query( $sql );
 if ( !$res ) {
-    send_to_admin( "Error: CLEAN_COUNT_TOTAL_FAIL\n" . $db->lastErrorMsg() );
+    send_to_admin( "Error: CLEAN_COUNT_TOTAL_FAIL\n" . db_last_error( $db ) );
     $db->close();
     exit();
 }
-$log = 'In DB: ' . $res->fetchArray()[0] . "\n";
+$log = 'In DB: ' . db_fetch( $res )[0] . "\n";
 
 // Сколько опросов удалено юзерами
 $sql = "SELECT COUNT(poll_id) FROM polls WHERE state = 'deleted'";
 $res = $db->query( $sql );
 if ( !$res ) {
-    send_to_admin( "Error: CLEAN_COUNT_DELETED_FAIL\n" . $db->lastErrorMsg() );
+    send_to_admin( "Error: CLEAN_COUNT_DELETED_FAIL\n" . db_last_error( $db ) );
     $db->close();
     exit();
 }
-$deleted = $res->fetchArray()[0];
+$deleted = db_fetch( $res )[0];
 $log     = $log . 'Deleted: ' . $deleted . "\n";
 
 // Сколько опросов уже было помечено и их пора удалять
 $sql     = "SELECT COUNT(poll_id) FROM polls WHERE state = 'clean'";
 $res     = $db->query( $sql );
 if ( !$res ) {
-    send_to_admin( "Error: CLEAN_COUNT_CLEAN_FAIL\n" . $db->lastErrorMsg() );
+    send_to_admin( "Error: CLEAN_COUNT_CLEAN_FAIL\n" . db_last_error( $db ) );
     $db->close();
     exit();
 }
-$clean = $res->fetchArray()[0];
+$clean = db_fetch( $res )[0];
 $log   = $log . 'Clean: ' . $clean . "\n";
 
 
 // Удаление голосов у помеченных опросов
 $sql = "DELETE FROM votes WHERE votes.poll_id IN (SELECT polls.poll_id FROM polls INNER JOIN votes ON ( votes.poll_id = polls.poll_id ) WHERE polls.state='clean');";
 if ( !$db->exec( $sql ) ) {
-    send_to_admin( "Error: CLEAN_VOTES_DELETE_FAIL\n" . $db->lastErrorMsg() );
+    send_to_admin( "Error: CLEAN_VOTES_DELETE_FAIL\n" . db_last_error( $db ) );
     $db->close();
     exit();
 }
@@ -55,7 +60,7 @@ if ( !$db->exec( $sql ) ) {
 // Удаление помеченных опросов
 $sql   = "DELETE FROM polls WHERE state = 'clean'";
 if ( !$db->exec( $sql ) ) {
-    send_to_admin( "Error: CLEAN_POLLS_DELETE_FAIL\n" . $db->lastErrorMsg() );
+    send_to_admin( "Error: CLEAN_POLLS_DELETE_FAIL\n" . db_last_error( $db ) );
     $db->close();
     exit();
 }
@@ -63,7 +68,7 @@ if ( !$db->exec( $sql ) ) {
 // Помечаем удалённые
 $sql = "UPDATE polls SET state = 'clean' WHERE state = 'deleted'";
 if ( !$db->exec( $sql ) ) {
-    send_to_admin( "Error: CLEAN_MARK_FAIL\n" . $db->lastErrorMsg() );
+    send_to_admin( "Error: CLEAN_MARK_FAIL\n" . db_last_error( $db ) );
     $db->close();
     exit();
 }
@@ -74,10 +79,10 @@ if ( $clean > 0 or $deleted > 0 ) {
     $sql = "SELECT COUNT(poll_id) FROM polls";
     $res = $db->query( $sql );
     if ( $res ) {
-        $log = $log . "After in DB: " . $res->fetchArray()[0];
+        $log = $log . "After in DB: " . db_fetch( $res )[0];
         send_to_admin( $log );
     } else {
-        send_to_admin( "Error: CLEAN_COUNT_AFTER_FAIL\n" . $db->lastErrorMsg() );
+        send_to_admin( "Error: CLEAN_COUNT_AFTER_FAIL\n" . db_last_error( $db ) );
     }
 }
 
